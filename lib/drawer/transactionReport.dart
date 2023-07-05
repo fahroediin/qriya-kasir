b87b51bd8cd0e4c8c650df0163e59db2b81da1f5
fahroediin <fahroediinj@gmail.com>
2023-07-05 19:18:14 +0700

    Memperbaiki halaman report trans

--- lib/drawer/transactionReport.dart
+++ lib/drawer/transactionReport.dart
@@ -15,6 +15,9 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
   int countdDataPenjualan = 0;
   int jumlahTransaksi = 0; // Menyimpan jumlah transaksi
   int jumlahItemTerjual = 0; // Menyimpan jumlah item terjual
+  int jumlahTotalPendapatan = 0; // Menyimpan jumlah total pendapatan
+  int totalDiskon = 0; // Menyimpan total diskon yang diberikan
+  int totalPendapatanBersih = 0; // Menyimpan total pendapatan bersih
   List<Map<dynamic, dynamic>> rankingSparepart =
       []; // Menyimpan ranking sparepart
 
@@ -34,10 +37,12 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
         .startAt(formattedFirstDayOfMonth)
         .endAt(formattedLastDayOfMonth + '\u{f8ff}')
         .get();
-
     if (mounted) {
       if (snapshot.exists) {
         int totalJumlahItemTerjual = 0;
+        int totalHarga = 0;
+        int totalDiskonPenjualan = 0;
+
         Map<String, int> sparepartCountMap = {};
 
         Map<dynamic, dynamic> snapshotValue =
@@ -46,6 +51,8 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
           if (value is Map<dynamic, dynamic>) {
             String? idSparepart = value['idSparepart'] as String?;
             int? jumlahItem = int.tryParse(value['jumlahItem'].toString());
+            int? hargaAkhir = int.tryParse(value['hargaAkhir'].toString());
+            int? diskon = int.tryParse(value['totalDiskon'].toString());
 
             if (idSparepart != null && jumlahItem != null) {
               if (sparepartCountMap.containsKey(idSparepart)) {
@@ -57,15 +64,17 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
             }
 
             totalJumlahItemTerjual += jumlahItem ?? 0;
+            totalHarga += hargaAkhir ?? 0;
+            totalDiskonPenjualan += diskon ?? 0;
           }
         });
 
-        List<String> uniqueIdSpareparts = sparepartCountMap.keys.toList();
-
-        rankingSparepart = uniqueIdSpareparts.map((idSparepart) {
+        List<Map<String, dynamic>> rankingSparepartList =
+            sparepartCountMap.entries.map((entry) {
+          String idSparepart = entry.key;
+          int jumlahItem = entry.value;
           String namaSparepart = ''; // Ganti dengan nama sparepart yang sesuai
           String merkSparepart = ''; // Ganti dengan merk sparepart yang sesuai
-          int jumlahItem = sparepartCountMap[idSparepart] ?? 0;
 
           return {
             'idSparepart': idSparepart,
@@ -75,12 +84,16 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
           };
         }).toList();
 
-        rankingSparepart
+        rankingSparepartList
             .sort((a, b) => b['jumlahItem'].compareTo(a['jumlahItem']));
 
         setState(() {
           jumlahTransaksi = snapshot.children.length;
           jumlahItemTerjual = totalJumlahItemTerjual;
+          jumlahTotalPendapatan = totalHarga;
+          totalDiskon = totalDiskonPenjualan;
+          totalPendapatanBersih = totalHarga - totalDiskonPenjualan;
+          rankingSparepart = rankingSparepartList;
         });
       } else {
         // If snapshot does not exist or data is empty
@@ -89,6 +102,11 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
         });
       }
     }
+  }
+
+  String formatCurrency(int value) {
+    final format = NumberFormat("#,###");
+    return format.format(value);
   }
 
   @override
@@ -108,52 +126,177 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
       ),
       body: Column(
         children: [
+          Expanded(
+            child: Column(
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Laporan Bulan:',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () async {
+                                DateTime? pickedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: selectedDate,
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2025),
+                                );
+
+                                if (pickedDate != null) {
+                                  setState(() {
+                                    selectedDate = pickedDate;
+                                  });
+                                  fetchDataPenjualan();
+                                }
+                              },
+                              child: Icon(Icons.calendar_today),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Bulan',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Text(
+                                      DateFormat('MMMM yyyy', 'id_ID')
+                                          .format(selectedDate),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Jumlah Transaksi',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 18),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Text(
+                                      jumlahTransaksi.toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Jumlah Item Terjual',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 20),
+                                    child: Text(
+                                      jumlahItemTerjual.toString(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: rankingSparepart.isEmpty
+                      ? Center(
+                          child: Text('Data tidak ada'),
+                        )
+                      : ListView.builder(
+                          itemCount: rankingSparepart.length > 10
+                              ? 10
+                              : rankingSparepart.length,
+                          itemBuilder: (context, index) {
+                            final sparepart = rankingSparepart[index];
+                            return ListTile(
+                              title: Text(
+                                '${sparepart['namaSparepart']} - ${sparepart['merkSparepart']}',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                  'Jumlah Item Terjual: ${sparepart['jumlahItem']}'),
+                            );
+                          },
+                        ),
+                ),
+              ],
+            ),
+          ),
           Card(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: Column(
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Text(
-                        'Laporan Bulan:',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      GestureDetector(
-                        onTap: () async {
-                          DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: selectedDate,
-                            firstDate: DateTime(2020),
-                            lastDate: DateTime(2025),
-                          );
-
-                          if (pickedDate != null) {
-                            setState(() {
-                              selectedDate = pickedDate;
-                            });
-                            fetchDataPenjualan();
-                          }
-                        },
-                        child: Icon(Icons.calendar_today),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Text('Bulan:'),
-                      SizedBox(width: 10),
-                      Text(
-                        DateFormat('MMMM yyyy', 'id_ID').format(
-                            selectedDate), // Gunakan locale bahasa Indonesia
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Jumlah Total Pendapatan',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              'Rp ' + formatCurrency(jumlahTotalPendapatan),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -161,12 +304,20 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
                   SizedBox(height: 10),
                   Row(
                     children: [
-                      Text('Jumlah Transaksi:'),
-                      SizedBox(width: 10),
-                      Text(
-                        jumlahTransaksi.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Diskon yang diberikan',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              'Rp ' + formatCurrency(totalDiskon),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -174,12 +325,20 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
                   SizedBox(height: 10),
                   Row(
                     children: [
-                      Text('Jumlah Item Terjual:'),
-                      SizedBox(width: 10),
-                      Text(
-                        jumlahItemTerjual.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Pendapatan Bersih',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              'Rp ' + formatCurrency(totalPendapatanBersih),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -187,29 +346,6 @@ class _TransactionReportPageState extends State<TransactionReportPage> {
                 ],
               ),
             ),
-          ),
-          Divider(),
-          Expanded(
-            child: rankingSparepart.isEmpty
-                ? Center(
-                    child: Text('Data tidak ada'),
-                  )
-                : ListView.builder(
-                    itemCount: rankingSparepart.length > 10
-                        ? 10
-                        : rankingSparepart.length,
-                    itemBuilder: (context, index) {
-                      final sparepart = rankingSparepart[index];
-                      return ListTile(
-                        title: Text(
-                          '${sparepart['namaSparepart']} - ${sparepart['merkSparepart']}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                            'Jumlah Item Terjual: ${sparepart['jumlahItem']}'),
-                      );
-                    },
-                  ),
           ),
         ],
       ),
