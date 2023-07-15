ab8bec3ee5f778f166bd03e20a90dccd7aee6ab8
fahroediin <fahroediinj@gmail.com>
2023-07-15 17:52:49 +0700

    Perbaiki banyak hal

--- lib/pages/servis.dart
+++ lib/pages/servis.dart
@@ -309,12 +309,24 @@ class _ServisPageState extends State<ServisPage> {
       context: context,
       builder: (BuildContext context) {
         List<Map<String, dynamic>> selectedSpareparts = [];
-
         List<Map<dynamic, dynamic>> sparepartList = [];
         List<Map<dynamic, dynamic>> filteredSparepartList = [];
         TextEditingController jumlahItemController = TextEditingController();
         TextEditingController searchController =
             TextEditingController(); // Tambahkan controller untuk TextField pencarian
+
+        // Fungsi untuk memperbarui daftar sparepart berdasarkan pencarian
+        void updateFilteredSparepartList() {
+          filteredSparepartList = sparepartList.where((sparepart) {
+            String namaSparepart =
+                sparepart['namaSparepart'].toString().toLowerCase();
+            String specSparepart =
+                sparepart['specSparepart'].toString().toLowerCase();
+            String searchKeyword = searchController.text.toLowerCase();
+            return namaSparepart.contains(searchKeyword) ||
+                specSparepart.contains(searchKeyword);
+          }).toList();
+        }
 
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
@@ -331,18 +343,7 @@ class _ServisPageState extends State<ServisPage> {
                         labelText: 'Cari Sparepart',
                       ),
                       onChanged: (value) {
-                        filteredSparepartList =
-                            sparepartList.where((sparepart) {
-                          String namaSparepart = sparepart['namaSparepart']
-                              .toString()
-                              .toLowerCase();
-                          String specSparepart = sparepart['specSparepart']
-                              .toString()
-                              .toLowerCase();
-                          String searchKeyword = value.toLowerCase();
-                          return namaSparepart.contains(searchKeyword) ||
-                              specSparepart.contains(searchKeyword);
-                        }).toList();
+                        updateFilteredSparepartList();
                         setState(() {});
                       },
                     ),
@@ -372,7 +373,7 @@ class _ServisPageState extends State<ServisPage> {
                                 sparepartList
                                     .add(Map<dynamic, dynamic>.from(value));
                               });
-                              filteredSparepartList = sparepartList;
+                              updateFilteredSparepartList(); // Perbarui daftar sparepart berdasarkan data terbaru
                             }
 
                             if (filteredSparepartList.isEmpty) {
